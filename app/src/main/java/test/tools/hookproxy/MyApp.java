9abdea3d5f879e157d615cadab1585393dbeebd3
liuchonghui <liuchonghui@xiaomi.com>
2019-01-19 17:36:52 +0800

    commit changes.
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

---
+++ app/src/main/java/test/tools/hookproxy/MyApp.java
@@ -0,0 +1,10 @@
+package test.tools.hookproxy;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
